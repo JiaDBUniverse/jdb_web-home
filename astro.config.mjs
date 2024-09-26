45e1d33a22adba8d224ba94d946fa89f67f0ae15
zhaotong <1427730623@qq.com>
2024-09-26 21:58:34 +0800

    feat#记多宝官网搭建

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        title: '记多宝',
+        social: {
+            github: 'https://github.com/JiaDBUniverse/jdb_web-home',
+        },
+        customCss: [
+            // 你的 Tailwind 基础样式的相对路径
+            './src/tailwind.css',
+        ],
+        defaultLocale: 'root',
+        locales: {
+            root: {
+                label: '简体中文',
+                lang: 'zh-CN'
+            },
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+    }), tailwind({
+        // 禁用默认的基础样式
+        applyBaseStyles: false,
+    })],
+});
